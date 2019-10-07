8c930de7ad9906a88851ab47520851608a6c816e
Letizia Scuriatti <letizia.scuriatti@studenti.unicam.it>
2019-10-07 12:26:44 +0200

    add addPG

--- controller/playerController.js
+++ controller/playerController.js
@@ -1,31 +1,53 @@
 var sqlite3 = require('sqlite3').verbose();
 const database = './players_and_masters.db';
 
-exports.addPG = function(req, idpg, res, done) {
+exports.addPG = function(req, res) {
 
     //aggiunta di un pg alla collezione player 
 
     let db = new sqlite3.Database(database);
 
     var id_player = req.session.user;
-    var id_pg = pgAcquistabile.id;
+    var nomePg = req.body.nomePersonaggio;
+    var id_Pg;
+    db.get(
+        'SELECT * FROM characters WHERE name=?',
+        nomePg,
+        function(err, row) {
+            console.log(row);
+            if (row != undefined) {
+                id_Pg = row.id;
+                console.log('id_Pg' + id_Pg);
+                db.get(
+                    'SELECT * FROM users_characters WHERE user_id = ? AND char_id=?',
+                    id_player,
+                    id_Pg,
+                    function(err, row) {
+                        console.log(id_Pg);
+                        if (row != undefined) {
+                            console.log(row);
+                            console.log('pg già posseduto');
+                            res.redirect('/player/playerhome');
 
-    db.each(
-        'SELECT * FROM user_characters WHERE user_id = ?',
-        id_player,
-        function(row) {
+                            //db.run('INSERT INTO users_character(user_id, char_id) VALUES (?, ?);', id_player, id_pg)
 
-            if (row.char_id != id_pg) {
+                        } else {
+                            console.log('pg non posseduto');
+                            db.run('INSERT INTO users_characters (user_id, char_id) VALUES (?,?);', id_player, id_Pg);
+                            res.redirect('/player/playerhome');
 
-                db.run('INSERT INTO users_character(user_id, char_id) VALUES (?, ?);', id_player, id_pg)
+                            // return done(null, false, { message: 'Personaggio selezionato già aggiunto alla collezione' })
 
+                        }
+                    }
+                );
             } else {
-
-                return done(null, false, { message: 'Personaggio selezionato già aggiunto alla collezione' })
-
+                console.log('pg inesistente')
+                res.redirect('/player/home');
             }
         }
     );
+
 
     db.close();
 
